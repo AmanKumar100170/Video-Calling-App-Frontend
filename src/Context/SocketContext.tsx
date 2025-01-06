41f2ff8e04ab7add0274a5347f2920ed748c6b81
Aman Kumar <aman100170@gmail.com>
2025-01-06 09:58:02 +0530

    Added logic of keeping record of users in a room

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,6 +1,8 @@
 import SocketIoClient from "socket.io-client";
-import React, { createContext, useEffect } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Peer from "peerjs";
+import { v4 as UUIDv4 } from 'uuid';
 
 const WS_Server = "http://localhost:5500";
 
@@ -18,17 +20,31 @@ interface Props {
 export const SocketProvider: React.FC<Props> = ({ children }) => {
 
     const navigate = useNavigate();
+
+    const [user, setUser] = useState<Peer>();
+
+    const fetchParticipantList = ({ participants, roomId } : { participants: string[], roomId: string }) => {
+        console.log('Current Room Id: ', roomId);
+        console.log('Current Participants: ', participants);
+    }
+
     useEffect(() => {
+
+        const userId = UUIDv4();
+        const newPeer = new Peer(userId);
+        setUser(newPeer);
 
         const enterRoom = ({ roomId } : { roomId: string }) => {
             navigate(`/room/${roomId}`);
         }
 
         socket.on('room-created', enterRoom);
+
+        socket.on('get-users', fetchParticipantList);
     }, []);
 
     return (
-        <SocketContext.Provider value={{ socket }}>
+        <SocketContext.Provider value={{ socket, user }}>
 
             {children}
             

f2332f331ff7e6a7eaa2c53d31d7716e2d1360cf
Aman Kumar <aman100170@gmail.com>
2025-01-06 14:04:30 +0530

    Added the facility of making a call with others

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,8 +1,10 @@
 import SocketIoClient from "socket.io-client";
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useEffect, useReducer, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Peer from "peerjs";
 import { v4 as UUIDv4 } from 'uuid';
+import { peerReducer } from "../Reducers/peerReducer";
+import { addPeerAction } from "../Actions/peerAction";
 
 const WS_Server = "http://localhost:5500";
 
@@ -24,6 +26,8 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
     const [user, setUser] = useState<Peer>();
 
     const [stream, setStream] = useState<MediaStream>();
+
+    const [peers, dispatch] = useReducer(peerReducer, {});
 
     const fetchParticipantList = ({ participants, roomId } : { participants: string[], roomId: string }) => {
         console.log('Current Room Id: ', roomId);
@@ -56,8 +60,34 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
         socket.on('get-users', fetchParticipantList);
     }, []);
 
+    useEffect(() => {
+        if(!user || !stream) return;
+
+        socket.on('user-joined', ({ peerId }) => {
+            const call = user.call(peerId, stream);
+            console.log('Calling the new peer:', peerId);
+
+            call.on('stream', () => {
+                dispatch(addPeerAction(peerId, stream));
+            })
+        })
+
+        user.on('call', (call) => {
+            // tells what to do when the other peers on the group, call the user when user joined
+            console.log('Received a call from peers');
+            call.answer(stream);
+
+            call.on('stream', () => {
+                dispatch(addPeerAction(call.peer, stream));
+            })
+        })
+
+        socket.emit('ready'); 
+
+    }, [user, stream])
+
     return (
-        <SocketContext.Provider value={{ socket, user, stream }}>
+        <SocketContext.Provider value={{ socket, user, stream, peers }}>
 
             {children}
             
